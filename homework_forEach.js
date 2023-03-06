b67e5821cfa5e227dc2173b3cf88110ae8a9f784
Roman Kutselepa <roman.kutselepa@gmail.com>
2023-03-06 11:25:03 +0200

    first commit

---
+++ homework_forEach.js
@@ -0,0 +1,49 @@
+// Добавьте в массив 'fullNames' имя и фамилию
+// из массивов 'names' и 'surnames'
+const names = ['Jack', 'Samantha', 'Bob', 'Mary', 'Simon', 'Jessie', 'Derek'];
+const surnames = ['Smith', 'Green', 'Summers', 'Wilson', 'Rose', 'Smart', 'Jones'];
+const fullNames = [];
+
+
+// Используйте forEach чтобы найти сумму всех чисел в массиве
+const numbers = [6, 4, 2, 1, 8, 9];
+
+
+// Используйте forEach чтобы в массив 'upperWords'
+// добавить те же слова, но в верхнем регистре
+const words = ['hello', 'people', 'javascript', 'frontend'];
+const upperWords = [];
+
+
+// Используйте forEach чтобы в массив 'reversedNumberRow'
+// добавить те же числа, но в обратном порядке.
+const numberRow = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let reversedNumberRow = [];
+
+
+// Для каждой машины в массиве используйте "forEach"
+// что-бы вывести в консоль строку типа:
+// This is green Jaguar. It costs 7000€.
+
+const cars = [
+    {
+        make: 'BMW',
+        color: 'grey',
+        price: 10000
+    },
+    {
+        make: 'Mercedes-Benz',
+        color: 'black',
+        price: 15000
+    },
+    {
+        make: 'Honda',
+        color: 'blue',
+        price: 8000
+    },
+    {
+        make: 'Ferrari',
+        color: 'red',
+        price: 50000
+    } 
+]
